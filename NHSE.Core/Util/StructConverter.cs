61a7fa2dbfba387add64c6a7370800b4ce8ff2cc
Kurt <kaphotics@gmail.com>
2020-03-29 17:18:36 -0700

    Add xunit test project
    ensures item sizes are correctly marshalling

--- NHSE.Core/Util/StructConverter.cs
+++ NHSE.Core/Util/StructConverter.cs
@@ -7,7 +7,7 @@ namespace NHSE.Core
     /// <summary>
     /// Logic for converting raw data to class/stack struct, and back to data.
     /// </summary>
-    internal static class StructConverter
+    public static class StructConverter
     {
         public static T ToStructure<T>(this byte[] bytes) where T : struct
         {
---
+++ NHSE.Tests/ItemTests.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using NHSE.Core;
+using Xunit;
+
+namespace NHSE.Tests
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void ItemMarshal()
+        {
+            var item = new Item();
+            var bytes = item.ToBytesClass();
+            bytes.Length.Should().Be(Item.SIZE);
+        }
+
+        [Fact]
+        public void VillagerItemMarshal()
+        {
+            var item = new VillagerItem();
+            var bytes = item.ToBytesClass();
+            bytes.Length.Should().Be(VillagerItem.SIZE);
+        }
+    }
+}
