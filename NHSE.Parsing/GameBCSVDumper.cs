46840b7125f6246d00edd88b64c0ec3733c7b389
Kurt <kaphotics@gmail.com>
2020-04-01 12:12:10 -0700

    Update GameBCSVDumper.cs

--- NHSE.Parsing/GameBCSVDumper.cs
+++ NHSE.Parsing/GameBCSVDumper.cs
@@ -8,9 +8,10 @@ namespace NHSE.Parsing
 {
     public static class GameBCSVDumper
     {
-        public static byte[] GetItemTypeArray(string pathBCSV)
+        public static byte[] GetItemTypeArray(string pathBCSV, string fn = "ItemParam.bcsv")
         {
-            var data = File.ReadAllBytes(pathBCSV);
+            var path = Path.Combine(pathBCSV, fn);
+            var data = File.ReadAllBytes(path);
             var bcsv = new BCSV(data);
 
             var dict = bcsv.GetFieldDictionary();
@@ -37,6 +38,63 @@ namespace NHSE.Parsing
             foreach (var kvp in types)
                 result[kvp.Key] = (byte) kvp.Value;
 
+            return result;
+        }
+
+        public static List<string> GetRecipeList(string pathBCSV, string fn = "RecipeCraftParam.bcsv")
+        {
+            var path = Path.Combine(pathBCSV, fn);
+            var data = File.ReadAllBytes(path);
+            var bcsv = new BCSV(data);
+
+            var dict = bcsv.GetFieldDictionary();
+            var frecipe = dict[0x54706054];
+            var fitemID = dict[0x89A3482C];
+
+            var items = GameInfo.Strings.itemlist;
+
+            var result = new List<string>();
+            for (int i = 0; i < bcsv.EntryCount; i++)
+            {
+                var iid = bcsv.ReadValue(i, fitemID);
+                var ival = ushort.Parse(iid);
+
+                var rid = bcsv.ReadValue(i, frecipe);
+                var rval = ushort.Parse(rid);
+
+                var kvp = $"{{0x{rval:X3}, {ival:00000}}}, // {items[ival]}";
+                result.Add(kvp);
+            }
+
+            result.Sort();
+            return result;
+        }
+
+        public static List<string> GetMilestoneList(string pathBCSV, string fn = "EventFlagsLifeSupportAchievementParam.bcsv")
+        {
+            var path = Path.Combine(pathBCSV, fn);
+            var data = File.ReadAllBytes(path);
+            var bcsv = new BCSV(data);
+
+            var dict = bcsv.GetFieldDictionary();
+            var findex = dict[0x54706054];
+            var fname = dict[0x45F320F2];
+            var fjp = dict[0x85CF1615];
+
+            var result = new List<string>();
+            for (int i = 0; i < bcsv.EntryCount; i++)
+            {
+                var iid = bcsv.ReadValue(i, findex);
+                var ival = ushort.Parse(iid);
+
+                var name = bcsv.ReadValue(i, fname).TrimEnd('\0');
+                var jp = bcsv.ReadValue(i, fjp).TrimEnd('\0');
+
+                var kvp = $"{{{ival:00}, \"{name}\"}}, // {jp}";
+                result.Add(kvp);
+            }
+
+            result.Sort();
             return result;
         }
     }
