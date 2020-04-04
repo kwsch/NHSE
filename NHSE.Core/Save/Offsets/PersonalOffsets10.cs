92959fba6a734b8c1ef4514fff885244f061827f
Kurt <kaphotics@gmail.com>
2020-04-04 15:53:28 -0700

    Fix wallet offset
    https://github.com/Cuyler36/MyHorizons/commit/e2b4c6ac4974ab72e8318d01d4157cb9db4e6c84
    ty @Cuyler36 for letting me know the correct offset

--- NHSE.Core/Save/Offsets/PersonalOffsets10.cs
+++ NHSE.Core/Save/Offsets/PersonalOffsets10.cs
@@ -8,13 +8,13 @@
         public override int PersonalId => 0xB0A0;
         public override int EventFlagsPlayer => PersonalId + 0x38; // +0x18 from v1.0, +0x18 after player name start
         public override int Activity => 0xCF6C;
-        public override int Wallet => 0x11578;
         public override int NookMiles => 0x11570;
         public override int Photo => 0x11598;
 
         public override int Pockets1 => 0x35BD4;
         public override int Pockets2 => Pockets1 + (8 * Pockets1Count) + 0x18;
-        public override int Storage => Pockets2 + (8 * Pockets2Count) + 0x24;
+        public override int Wallet => Pockets2 + (8 * Pockets2Count) + 0x18;
+        public override int Storage => Wallet + 0xC;
         public override int ReceivedItems => 0x3FC1C;
 
         public override int Bank => 0x68BE4;
