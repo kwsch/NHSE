6bdab868eb67b645c36ab940d57c4c8c3f1f3850
Kurt <kaphotics@gmail.com>
2020-04-11 16:10:34 -0700

    Add enum hashing method

---
+++ NHSE.Tests/EnumHashTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NHSE.Parsing;
+using Xunit;
+
+namespace NHSE.Tests
+{
+    public class EnumHashTests
+    {
+        [Theory]
+        [InlineData("Base", 0x6086515F)]
+        [InlineData("River", 0x3422482F)]
+        [InlineData("RoadStone", 0x13011867)]
+        public void ChecksumMatches(string str, uint val)
+        {
+            var computed = CRC32.Compute(str);
+            computed.Should().Be(val);
+        }
+    }
+}
