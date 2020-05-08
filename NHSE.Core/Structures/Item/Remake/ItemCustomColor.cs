f6aab41872b71fd67f0974cc45e593b1dbc3a054
Kurt <kaphotics@gmail.com>
2020-05-07 20:34:22 -0700

    Remove extra ' in comment

--- NHSE.Core/Structures/Item/Remake/ItemCustomColor.cs
+++ NHSE.Core/Structures/Item/Remake/ItemCustomColor.cs
@@ -2,20 +2,20 @@
 {
     public enum ItemCustomColor : byte
     {
-        Orange = 0,    // 'オレンジ
-        Red = 1,       // 'レッド
-        Pink = 2,      // 'ピンク
-        Purple = 3,    // 'パープル
-        Blue = 4,      // 'ブルー
-        LightBlue = 5, // 'みずいろ
-        Green = 6,     // 'グリーン
-        Yellow = 7,    // 'イエロー
-        Beige = 8,     // 'ベージュ
-        Brown = 9,     // 'ブラウン
-        Black = 10,    // 'ブラック
-        Gray = 11,     // 'グレー
-        White = 12,    // 'ホワイト
-        Colorful = 13, // 'カラフル
-        None = 14,     // 'なし
+        Orange = 0,    // オレンジ
+        Red = 1,       // レッド
+        Pink = 2,      // ピンク
+        Purple = 3,    // パープル
+        Blue = 4,      // ブルー
+        LightBlue = 5, // みずいろ
+        Green = 6,     // グリーン
+        Yellow = 7,    // イエロー
+        Beige = 8,     // ベージュ
+        Brown = 9,     // ブラウン
+        Black = 10,    // ブラック
+        Gray = 11,     // グレー
+        White = 12,    // ホワイト
+        Colorful = 13, // カラフル
+        None = 14,     // なし
     }
 }
