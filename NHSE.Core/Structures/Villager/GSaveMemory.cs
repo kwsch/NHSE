adda3c0bb404e30454ac31f198a7bead1688f4f6
Kurt <kaphotics@gmail.com>
2020-05-25 08:42:35 -0700

    Fix villager origin check
    Closes #252

--- NHSE.Core/Structures/Villager/GSaveMemory.cs
+++ NHSE.Core/Structures/Villager/GSaveMemory.cs
@@ -23,7 +23,7 @@ namespace NHSE.Core
             get => StringUtil.GetString(Data, 0x04, 10);
             set => StringUtil.GetBytes(value, 10).CopyTo(Data, 0x04);
         }
-        public byte[] GetTownIdentity() => Data.Slice(0x00, 20);
+        public byte[] GetTownIdentity() => Data.Slice(0x00, 4 + 20);
 
         public uint PlayerID
         {
@@ -37,7 +37,7 @@ namespace NHSE.Core
             set => StringUtil.GetBytes(value, 10).CopyTo(Data, 0x20);
         }
 
-        public byte[] GetPlayerIdentity() => Data.Slice(0x1C, 20);
+        public byte[] GetPlayerIdentity() => Data.Slice(0x1C, 4 + 20);
 
         public byte[] GetEventFlags() => Data.Slice(0x38, 0x100);
         public void SetEventFlags(byte[] value) => value.CopyTo(Data, 0x38);
