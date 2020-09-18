9ed5cfb62e2ef55578f034a5be34b561c33601df
Charlie U <dzcharlie@gmail.com>
2020-09-17 21:21:40 -0400

    Add event handler for count label click

--- NHSE.WinForms/Controls/ItemEditor.Designer.cs
+++ NHSE.WinForms/Controls/ItemEditor.Designer.cs
@@ -154,6 +154,7 @@
             this.L_Count.TabIndex = 7;
             this.L_Count.Text = "Count:";
             this.L_Count.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.L_Count.Click += new System.EventHandler(this.L_Count_Click);
             // 
             // L_Uses
             // 
