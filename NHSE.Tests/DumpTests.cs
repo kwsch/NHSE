3d3f84f5e2d4d577cb99c381625a3b17cf8849d5
Kurt <kaphotics@gmail.com>
2021-01-27 17:58:17 -0800

    Dump item names

--- NHSE.Tests/DumpTests.cs
+++ NHSE.Tests/DumpTests.cs
@@ -10,7 +10,7 @@ namespace NHSE.Tests
         private const string RepoPath = @"C:\Users\Kurt\Documents\GitHub";
         private const string PatchDumpPath = @"D:\Kurt\Desktop\" + PatchFolderName;
         private const string PatchFolderName = "v17";
-        private const string MessageDumpFormat = @"\Message\String_{0}.sarc";
+        private const string MessageDumpFormat = @"\Message\String_{0}";
 
         [Fact]
         public static void DumpBCSV()
