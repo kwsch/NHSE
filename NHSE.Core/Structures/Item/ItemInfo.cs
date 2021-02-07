c522bc9022368a3682a1c8fd07c5227d56cf67a7
Kurt <kaphotics@gmail.com>
2021-02-07 15:57:45 -0800

    Update ItemInfo.cs
    for whatever reason, our ItemInfo binary assumes most flowers are uniticon* rather than just Flower; just remap.

--- NHSE.Core/Structures/Item/ItemInfo.cs
+++ NHSE.Core/Structures/Item/ItemInfo.cs
@@ -50,6 +50,8 @@ namespace NHSE.Core
         public static bool TryGetMaxStackCount(ushort id, out ushort max)
         {
             var kind = GetItemKind(id);
+            if (kind.IsFlowerPicked())
+                kind = Kind_Flower;
             return MaxCountByKind.TryGetValue(kind, out max);
         }
 
