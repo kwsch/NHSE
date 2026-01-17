3d9d10bb975a0f505d8a6ede39dad2a396578be0
DESKTOP-SN9KF5O\Strawberry <berichandev@gmail.com>
2026-01-17 02:34:59 +0000

    Clean up imagefetcher for net10/cross thread safety

---
+++ NHSE.WinForms/Util/CrossThreadExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NHSE.WinForms;
+
+public static class CrossThreadExtensions
+{
+    /// <summary>
+    /// Helper function to perform an action on a Control's thread safely.
+    /// </summary>
+    public static void PerformSafely(this Control target, Action action)
+    {
+        if (target.InvokeRequired)
+        {
+            target.Invoke(action);
+        }
+        else
+        {
+            action();
+        }
+    }
+}
