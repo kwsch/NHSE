b62ad47dcad65c4b6a86ec3aaa9a0e1c5bbb2b27
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2026-05-19 23:25:39 +0200

    Add ItemClothGroup, ItemColorOrder and ItemSortOrder

---
+++ NHSE.Core/Structures/Item/ItemColorOrder.cs
@@ -0,0 +1,24 @@
+﻿namespace NHSE.Core;
+
+/// <summary>
+/// Color sort order for Items
+/// Provided by Ninji, not taken from the game: https://discord.com/channels/701573691426996324/701575987854311434/720271351314907148
+/// </summary>
+public enum ItemColorOrder : byte
+{
+    White,
+    Gray,
+    Black,
+    Red,
+    Pink,
+    Orange,
+    Yellow,
+    Green,
+    LightBlue,
+    Blue,
+    Purple,
+    Brown,
+    Beige,
+    Colorful,
+    None
+}
